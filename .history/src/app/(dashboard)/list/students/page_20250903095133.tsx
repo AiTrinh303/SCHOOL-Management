c64a4a3b3c0ef23dabe3824fc23b6840251956be
Ai Trinh <trinhnguyentrongai@gmail.com>
2025-09-03 10:04:57 +0200

    add parents list page

--- .history/src/app/(dashboard)/list/students/page_20250903095133.tsx
+++ .history/src/app/(dashboard)/list/students/page_20250903095133.tsx
@@ -52,7 +52,7 @@ const StudentListPage = () => {
   const renderRow = (item: Student) => (
     <tr
       key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-jojoPurpleLight"
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">
         <Image
@@ -79,7 +79,7 @@ const StudentListPage = () => {
             </button>
           </Link>
           {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-jojoPurple">
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
               <Image src="/delete.png" alt="" width={16} height={16} />
             </button>
           )}
@@ -96,14 +96,14 @@ const StudentListPage = () => {
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-jojoYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-jojoYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-jojoYellow">
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
                 <Image src="/plus.png" alt="" width={14} height={14} />
               </button>
             )}
