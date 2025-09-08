4a118a04bebcb1069aea03814678573300a909a0
Ai Trinh <trinhnguyentrongai@gmail.com>
2025-09-08 11:10:44 +0200

    fix

--- .history/next.config_20250908110311.mjs
+++ .history/next.config_20250908110311.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     images:{
         remotePatterns: [
             {
-                protocol: 'https',
-      hostname: 'images.pexels.com',
+                hostname: 'images.pexels.com',
+                domains: ["images.pexels.com"],
             },
         ],
     },
